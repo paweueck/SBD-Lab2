2222e0090ab7ccc33847806e028f357052cb29de
paweueck <paweueck@gmail.com>
2017-10-09 17:45:30 +0200

    correcting local run script

--- run_loc_test.sh
+++ run_loc_test.sh
@@ -4,5 +4,5 @@
 # sudo chmod 777 run.sh
 
 python3 phone_number_analysis.py \
---input file:/home/paweueck/scala-workspace/SBD_Lab2_Group20/crawl-data/CC-MAIN-2017-13/segments/1490218186353.38/wet/CC-MAIN-20170322212946-00000-ip-10-233-31-227.ec2.internal.warc.wet.gz \
+--input file:/home/paweueck/scala-workspace/SBD_Lab2_Group20/input/test_wet.txt \
 --output file:/home/paweueck/scala-workspace/SBD_Lab2_Group20/output/
